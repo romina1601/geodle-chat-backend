468f965611466ea8c1ade958e4aa39917d0999a1
romina1601 <romina.baila16@gmail.com>
2025-03-21 19:51:17 +0200

    🧑‍💻 Load env variables written to .env file, so you don't have to manually add them at every run

--- geodle_chat/app/main.py
+++ geodle_chat/app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from geodle_chat.api.endpoints import router
+from geodle_chat.core.config import settings
 
 app = FastAPI(title="Geodle Chat Backend")
 
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # TODO: Adjust this after deploying
+    allow_origins=[settings.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- geodle_chat/core/config.py
+++ geodle_chat/core/config.py
@@ -1,6 +1,12 @@
 import os
 
+from dotenv import load_dotenv
+
+# Load the environment variables from a .env file
+load_dotenv()
+
 class Settings:
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
+    FRONTEND_URL: str = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 settings = Settings()
