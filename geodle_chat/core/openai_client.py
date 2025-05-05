84e32080b68a7d66bc736729a8f16a934e41301d
romina1601 <romina.baila16@gmail.com>
2025-05-05 23:51:39 +0300

    🚸 Switch to gpt 4.1 mini model for better answers and limit the length of output

--- geodle_chat/core/openai_client.py
+++ geodle_chat/core/openai_client.py
@@ -9,12 +9,13 @@ if not api_key:
 
 client = AsyncOpenAI(api_key=api_key)
 
-async def generate_answer_stream(conversation: list, model: str = "gpt-4o-mini", temperature: float = 1) -> str:
+async def generate_answer_stream(conversation: list, model: str = "gpt-4.1-mini", temperature: float = 1) -> str:
     response = await client.chat.completions.create(
         model=model,
         messages=conversation,
         temperature=temperature,
-        stream=True  # Make sure response is returned as stream
+        max_completion_tokens=80,
+        stream=True # Make sure response is returned as stream
     )
     async for chunk in response:
         delta = chunk.choices[0].delta
